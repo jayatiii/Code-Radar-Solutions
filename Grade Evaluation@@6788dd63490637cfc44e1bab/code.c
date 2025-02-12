5c688adca17d1ad796352658cc9ddddc411c393b
Jayati Verma <vjayati21@gmail.com>
2025-02-12 23:56:48 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -8,4 +8,16 @@ int main(){
     else if (a == 'B'){
         printf("Good");
     }
+    else if (a == 'C'){
+        printf("Average");
+    }
+    else if (a == 'D'){
+        printf("Below Average");
+    }
+    else if (a == 'F'){
+        printf("Fail");
+    }
+    else{
+        printf("Invalid grade");
+    }
 }
