0a6fb97d419f844c6d42fa41b15b653676df9e77
Jayati Verma <vjayati21@gmail.com>
2025-02-12 23:55:23 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    char a;
+    scanf("hd; %c", &a);
+    if (a == 'A'){
+        printf("Excellent");
+    }
+    else if (a == 'B'){
+        printf("Good");
+    }
+}
